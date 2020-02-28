e7485f2247dbf79a91193063231d23c24eb9922b
Sankar S <san230693@gmail.com>
2020-02-28 23:32:16 +0530

    Pick image code included not working

--- app/src/main/java/com/learnprogramming/learningtasks/MainActivity.kt
+++ app/src/main/java/com/learnprogramming/learningtasks/MainActivity.kt
@@ -1,44 +1,192 @@
 package com.learnprogramming.learningtasks
 
+import android.annotation.TargetApi
+import android.content.ContentUris
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.DocumentsContract
+import android.provider.MediaStore
 import android.util.Log
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.google.android.material.textfield.TextInputEditText
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     val TAG = "MainActivity"
+    //Our variables
+    private var mImageView: ImageView? = null
+    private var mUri: Uri? = null
+    //Our widgets
+    private lateinit var btnCapture: Button
+    private lateinit var btnChoose: Button
+    //Our constants
+    private val OPERATION_CAPTURE_PHOTO = 1
+    private val OPERATION_CHOOSE_PHOTO = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
+        //   login()
+        initializeWidgets()
 
+        btnCapture.setOnClickListener { capturePhoto() }
+        btnChoose.setOnClickListener {
+            //check permission at runtime
+            val checkSelfPermission = ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            if (checkSelfPermission != PackageManager.PERMISSION_GRANTED) {
+                //Requests permissions to be granted to this application at runtime
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 1
+                )
+            } else {
+                openGallery()
+            }
+        }
+    }
+
+    fun login() {
         val loginbtn = findViewById(R.id.login) as Button
 
-        loginbtn.setOnClickListener{
-            Log.d(TAG,"loginbtn Clicked")
-
+        loginbtn.setOnClickListener {
+            Log.d(TAG, "loginbtn Clicked")
+            hideKeyboard()
             var textViewUsername = findViewById<TextInputEditText>(R.id.usernameInputBox)
             var textViewPassword = findViewById<TextInputEditText>(R.id.passwordInputBox)
             var userName = textViewUsername.text.toString()
             var password = textViewPassword.text.toString()
 
-            if(userName != "" && password != "")
-            {
-                if(userName == "Testing" && password == "123456789"){
-                    Log.d(TAG,"userName : $userName password $password")
-                    Toast.makeText(this,"userName : $userName password $password",Toast.LENGTH_SHORT).show()
+            if (userName != "" && password != "") {
+                if (userName == "Testing" && password == "123456789") {
+                    Log.d(TAG, "userName : $userName password $password")
+                    Toast.makeText(
+                        this,
+                        "userName : $userName password $password",
+                        Toast.LENGTH_SHORT
+                    ).show()
                     setContentView(R.layout.activity_main)
-                }else{
-                    Toast.makeText(this,"Username and password mismatch",Toast.LENGTH_SHORT).show()
-
+                } else {
+                    Toast.makeText(this, "Username and password mismatch", Toast.LENGTH_SHORT)
+                        .show()
                 }
-            }
-            else
-            {
-                Toast.makeText(this,"Username and password Cannot be empty",Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Username and password Cannot be empty", Toast.LENGTH_SHORT)
+                    .show()
             }
         }
 
     }
+
+    fun hideKeyboard() {
+        val inputManager: InputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(
+            currentFocus!!.windowToken,
+            InputMethodManager.SHOW_FORCED
+        )
+    }
+
+    private fun initializeWidgets() {
+        btnCapture = findViewById(R.id.pickImage)
+        btnChoose = findViewById(R.id.CaptureImage)
+        mImageView = findViewById(R.id.Profile)
+    }
+
+    private fun show(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun capturePhoto() {
+        val capturedImage = File(externalCacheDir, "My_Captured_Photo.jpg")
+        if (capturedImage.exists()) {
+            capturedImage.delete()
+        }
+        capturedImage.createNewFile()
+        mUri = if (Build.VERSION.SDK_INT >= 24) {
+            FileProvider.getUriForFile(
+                this, "info.camposha.kimagepicker.fileprovider",
+                capturedImage
+            )
+        } else {
+            Uri.fromFile(capturedImage)
+        }
+
+        val intent = Intent("android.media.action.IMAGE_CAPTURE")
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mUri)
+        startActivityForResult(intent, OPERATION_CAPTURE_PHOTO)
+    }
+
+    private fun openGallery() {
+        val intent = Intent("android.intent.action.GET_CONTENT")
+        intent.type = "image/*"
+        startActivityForResult(intent, OPERATION_CHOOSE_PHOTO)
+    }
+
+    private fun renderImage(imagePath: String?) {
+        if (imagePath != null) {
+            val bitmap = BitmapFactory.decodeFile(imagePath)
+            mImageView?.setImageBitmap(bitmap)
+        } else {
+            show("ImagePath is null")
+        }
+    }
+
+    private fun getImagePath(uri: Uri?, selection: String?): String {
+        var path: String? = null
+        val cursor = contentResolver.query(uri!!, null, selection, null, null)
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA))
+            }
+            cursor.close()
+        }
+        return path!!
+    }
+
+    @TargetApi(19)
+    private fun handleImageOnKitkat(data: Intent?) {
+        var imagePath: String? = null
+        val uri = data!!.data
+        //DocumentsContract defines the contract between a documents provider and the platform.
+        if (DocumentsContract.isDocumentUri(this, uri)) {
+            val docId = DocumentsContract.getDocumentId(uri)
+            if ("com.android.providers.media.documents" == uri!!.authority) {
+                val id = docId.split(":")[1]
+                val selsetion = MediaStore.Images.Media._ID + "=" + id
+                imagePath = getImagePath(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    selsetion
+                )
+            } else if ("com.android.providers.downloads.documents" == uri.authority) {
+                val contentUri = ContentUris.withAppendedId(
+                    Uri.parse(
+                        "content://downloads/public_downloads"
+                    ), java.lang.Long.valueOf(docId)
+                )
+                imagePath = getImagePath(contentUri, null)
+            }
+        } else if ("content".equals(uri!!.scheme, ignoreCase = true)) {
+            imagePath = getImagePath(uri, null)
+        } else if ("file".equals(uri.scheme, ignoreCase = true)) {
+            imagePath = uri.path
+        }
+        renderImage(imagePath)
+    }
+
+
 }
